505dd9fece92f65f5fc5ba7d0a465b68e580e736
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-05-18 17:32:31 +0000

    final website

--- js/script.js
+++ js/script.js
@@ -7,14 +7,23 @@
 "use strict"
 
 const randomNumber = Math.floor(Math.random() * (6 - -7)) + -7
-  // Generates a random number from -7 to 6 (equal number of negative and positive numbers, as 0 is counted as positive for simplicity)
+// Generates a random number from -7 to 6 (equal number of negative and positive numbers, as 0 is counted as positive for simplicity)
 let numberPolarity = null
+let polarityWord = ""
+let revealed = false
 
 if (randomNumber >= 0) {
-    numberPolarity = 1 // positive
-  } else {
-    numberPolarity = 0 // negative
-  }
+  numberPolarity = 1 // positive
+} else {
+  numberPolarity = 0 // negative
+}
+
+if (numberPolarity == 1) {
+  polarityWord = "positive"
+}
+if (numberPolarity == 0) {
+  polarityWord = "negative"
+}
 
 function guessPressed() {
   // a value of 1 means positive
@@ -22,31 +31,42 @@ function guessPressed() {
   let userGuess = null
   let positiveButtonChecked = document.getElementById("positive").checked
 
-  /** !!IMPORTANT!!
-   * it checks if the positive button is checked, but not if the negative button is checked. 
-   * This can cause issues vis-a-vis the system taking no input as a "negative" button press, as the system doesn't know whether or not "negative" is even pressed.
-   * This could also be fixed by setting a pre-selected value - as Mr. Coxall likely did - thereby making it impossible to simply give no answer.
-   *   -This solution would still be the same thing as the 'problem' I'm having, although it would be more 'official' of sorts.
-   *    -Additionally, instead of "negative" being the default option, "positive" would be.
-   */
-
   if (positiveButtonChecked === true) {
     /**
      * This checks if the positive radio button is selected, if it is, the user guessed "positive" (1).
-     * If not, the user guessed the "negative" button (0).
+     * If not, the user must have guessed the "negative" button (0).
      */
     userGuess = 1 // positive
-  } else if (positiveButtonChecked === false) {
-    userGuess = 0 // negative 
   } else {
-    userGuess = null
+    userGuess = 0 // negative
   }
 
   if (userGuess == numberPolarity) {
-      document.getElementById("answer").innerHTML =
-        "The answer is: numberPolarity: " + numberPolarity + "  randomNumber: " + randomNumber + "  userGuess: " + userGuess + "  CORRECT"
+    document.getElementById("answer").innerHTML =
+      "Your guess was correct! The number was " +
+      randomNumber +
+      ", which is " +
+      polarityWord +
+      "."
   } else {
-      document.getElementById("answer").innerHTML =
-    "The answer is: numberPolarity: " + numberPolarity + "  randomNumber: " + randomNumber + "  userGuess: " + userGuess + "  WRONG"
+    document.getElementById("answer").innerHTML =
+      "Try again, your guess was incorrect."
+  }
+}
+
+function revealAnswerPressed() {
+  // this is activated when the user presses the "reveal answer" button, then showing the answer and polarity
+  // the "revealed" boolean allows this button to be toggleable
+  if (revealed === false) {
+    document.getElementById("revealAnswer").innerHTML =
+      "The random number is " +
+      randomNumber +
+      ", which is " +
+      polarityWord +
+      "."
+    revealed = true
+  } else if (revealed === true) {
+    document.getElementById("revealAnswer").innerHTML = ""
+    revealed = false
   }
 }
