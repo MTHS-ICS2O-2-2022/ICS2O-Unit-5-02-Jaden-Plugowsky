2cd5ad4a5ef292831bb0ef81c4d245e06304b7cc
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-05-20 01:37:06 +0000

    go completed

--- go_app/main.go
+++ go_app/main.go
@@ -13,36 +13,31 @@ import (
 
 var source = rand.NewSource(time.Now().UnixNano())
 var random = rand.New(source)
-var randomNumber = random.Intn(5)
+var randomNumber = (random.Intn(6 - -6) + -6)
 var numberPolarity int
-var userGuess 
-
-if randomNumber >= 0 {
-  numberPolarity = 1 // positive
-} else {
-  numberPolarity = 0 // negative
-}
-
-/* IMPORTANT
-Go is incomplete here, and will stay incomplete until I have more time to do it, if that ever comes to begin with.
-*/
+var userGuess int
 
 func main() {
-	// This function finds a random number, then it asks the user to guess its polarity
-	var userGuess int
+	// This function finds a random number, then asking the user to guess whether the number is positive or negative
+	if randomNumber >= 0 {
+		numberPolarity = 1 // positive
+	} else {
+		numberPolarity = 0 // negative
+	}
 
 	// Input
-	fmt.Println("\nThis program finds a random number, then asking the user to guess the random number.")
-	fmt.Println()
-	fmt.Print("Enter your guess from 0-5: ")
+	fmt.Println("\nThis program finds a random number, then asking the user to guess whether the number is positive or negative.")
+	fmt.Println("Type '1' if you think the number is positive, type '0' if you think the number is negative. Any other inputs are invalid.")
+	fmt.Println("\nEnter your guess, 1 (positive) or 0 (negative): ")
 	fmt.Scanln(&userGuess)
 
 	// Process
-	if userGuess == randomNumber {
-		fmt.Println("\nYou have guessed the correct number, which was", randomNumber)
+	if userGuess == numberPolarity {
+		fmt.Println("\nYou have guessed correctly! The number was", randomNumber)
 	}
-	if userGuess != randomNumber {
-		fmt.Println("\nYou have guessed the number incorrectly, which was", randomNumber)
+	if userGuess != numberPolarity {
+		fmt.Println("\nYou have either guessed the number incorrectly, or you have entered an invalid response.")
+		fmt.Println("The number was ", randomNumber)
 	}
 
 	// Output
